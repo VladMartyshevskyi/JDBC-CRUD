ccb5d206b10f54ce7697fd90f960dc42d72e06cd
Vlad <vladmartishevskii@gmail.com>
2018-05-09 19:29:00 +0300

    Added CRUD

--- src/main/java/com/vlad/JdbcCrudApplication.java
+++ src/main/java/com/vlad/JdbcCrudApplication.java
@@ -5,11 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.vlad.dao.UserDao;
+import com.vlad.dao.jdbc.JdbcTemplateUserDaoImpl;
+
 @SpringBootApplication
+@Configuration
 public class JdbcCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcCrudApplication.class, args);
 	}
 	
+	@Bean
+    public UserDao AuthenticationFilter() {
+        return new JdbcTemplateUserDaoImpl();
+    }
 }
---
+++ src/main/java/com/vlad/models/User.java
@@ -0,0 +1,46 @@
+package com.vlad.models;
+
+public class User {
+	
+	private Integer id;
+	private String name;
+	private String lastName;
+	private Role role;
+	
+	public User() {
+	}
+	
+	public User(Integer id, String name, String lastName, Role role) {
+		this.id = id;
+		this.name = name;
+		this.lastName = lastName;
+		this.role = role;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+}
